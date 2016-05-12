32e3c79e1ad70010bdb283364abbf75c2068ed2f
s1240041 <s1240041@u-aizu.ac.jp>
2016-05-12 19:45:57 +0900

    solve case problems

---
+++ ex-case.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+loop{
+
+  res = 0
+
+  a,op,b = STDIN.gets.split(' ')
+  a = a.to_i
+  b = b.to_i  
+
+  case op
+
+    when '+'
+      puts a + b
+    when '-'
+      puts a - b
+    when '*'
+      puts a * b
+    when '/'
+      puts a / b
+    when '?'
+      break
+end
+}
